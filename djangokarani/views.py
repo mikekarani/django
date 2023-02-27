86a07e4660e989864b20a6a788dd96b6a6c9b58b
mikekarani <mikekarani421@gmail.com>
2023-02-27 14:00:32 +0300

    django

---
+++ djangokarani/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+
+def welcome(request):
+    return HttpResponse("Welcome to Django class")
+def eMobilis(request):
+    return HttpResponse("eMobilis mobile technological institute or academy")
+def wanjapi(request):
+    return HttpResponse("Kula luku kulaa wanjaaaapiiii.....aaaaaa...wanjapi")
+def ars(request):
+    return HttpResponse("The epl tittle holders of 2023-2024 season shall be Arsenallllllll")
+def swatch(request):
+    return HttpResponse("Sleep should not derail you from achieving the best")
